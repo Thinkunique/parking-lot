8b0e909d79a0e1764c3c4608190a2571fb32c5d9
Admin <Admin@host.docker.internal>
2020-07-02 15:19:50 +0530

    refactoring

--- ParkingLot/src/com/parkinglot/repo/impl/ParkingRepositoryImpl.java
+++ ParkingLot/src/com/parkinglot/repo/impl/ParkingRepositoryImpl.java
@@ -17,50 +17,46 @@ public class ParkingRepositoryImpl implements ParkingRepository {
 	ParkingSpace parkingSpace;
 
 	Map<String, ColorGroup> colorsMap;
-	
+
 	Map<String, String> regMap;
 
 	@Override
 	public void createNewParkingLot(int size) {
 		this.parkingSpace = new ParkingSpace(size);
 		this.colorsMap = new HashMap<>();
-		this.regMap=new HashMap<>();
+		this.regMap = new HashMap<>();
 	}
 
 	@Override
 	public void allocateParkingSlot(ParkingSlot slot) {
-		if (!parkingSpace.isFull()) {
-			Integer availableSlot = parkingSpace.getAvailableSlots().pollFirst();
-			parkingSpace.getSlots()[availableSlot] = slot;
-			String slotNumber=String.valueOf((availableSlot + 1));
-			Car car = slot.getCar();
-			this.regMap.put(car.getRegistration_number(),slotNumber);
-			ColorGroup group = this.colorsMap.get(car.getColor());
-			if (group != null) {
-				group.getRegNum().add(car.getRegistration_number());
-				group.getSlotNum().add(slotNumber);
-			} else {
-				group = ColorGroupUtility.getGroup();
-				group.getRegNum().add(car.getRegistration_number());
-				group.getSlotNum().add(slotNumber);
-				this.colorsMap.put(car.getColor(), group);
-			}
+		Integer availableSlot = parkingSpace.getAvailableSlots().pollFirst();
+		parkingSpace.getSlots()[availableSlot] = slot;
+		String slotNumber = String.valueOf((availableSlot + 1));
+		Car car = slot.getCar();
+		this.regMap.put(car.getRegistration_number(), slotNumber);
+		ColorGroup group = this.colorsMap.get(car.getColor());
+		if (group != null) {
+			group.getRegNum().add(car.getRegistration_number());
+			group.getSlotNum().add(slotNumber);
 		} else {
-			System.out.println("full");
+			group = ColorGroupUtility.getGroup();
+			group.getRegNum().add(car.getRegistration_number());
+			group.getSlotNum().add(slotNumber);
+			this.colorsMap.put(car.getColor(), group);
 		}
 	}
 
 	@Override
 	public void deallocateParkingSlot(int slotNumber) {
-		parkingSpace.getAvailableSlots().add(slotNumber-1);
-		ParkingSlot slot=parkingSpace.getSlots()[slotNumber-1];
-		Car car=slot.getCar();
-		List<String> list=this.colorsMap.get(car.getColor()).getRegNum();
-		List<String> list2=this.colorsMap.get(car.getColor()).getSlotNum();
+		parkingSpace.getAvailableSlots().add(slotNumber - 1);
+		ParkingSlot slot = parkingSpace.getSlots()[slotNumber - 1];
+		Car car = slot.getCar();
+		List<String> list = this.colorsMap.get(car.getColor()).getRegNum();
+		List<String> list2 = this.colorsMap.get(car.getColor()).getSlotNum();
 		list.remove(car.getRegistration_number());
 		list2.remove(slot.getSlotNumber());
 		regMap.remove(car.getRegistration_number());
-		parkingSpace.getSlots()[slotNumber-1] = null;
+		parkingSpace.getSlots()[slotNumber - 1] = null;
 	}
 
 	@Override
@@ -75,13 +71,13 @@ public class ParkingRepositoryImpl implements ParkingRepository {
 
 	@Override
 	public List<String> getRegistrationNumbersForCarColor(String color) {
-		ColorGroup group=this.colorsMap.get(color);
+		ColorGroup group = this.colorsMap.get(color);
 		return group.getRegNum();
 	}
 
 	@Override
 	public List<String> getSlotNumbersForCarColor(String color) {
-		ColorGroup group=this.colorsMap.get(color);
+		ColorGroup group = this.colorsMap.get(color);
 		return group.getSlotNum();
 	}
 
